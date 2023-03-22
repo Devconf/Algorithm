bd4ebff1d2d01cc4f9563b2334d0e96691a0b491
Hyunsik Kim <devconf5296@gmail.com>
2023-03-22 13:45:44 +0900

    구간 합 구하기 5

---
+++ BOJ/boj11660/소스.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int n, m;
+int dp[1025][1025];
+
+void solution()
+{
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1;j <= n; j++)
+		{
+			cin >> dp[i][j];
+		}
+	}
+
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= n; j++)
+		{
+			dp[i][j] = dp[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+		}
+	}
+
+	for(int i = 0; i < m; i++)
+	{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1]+dp[x1-1][y1-1]<<"\n";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	memset(dp, 0, sizeof(dp));
+	cin >> n >> m;
+
+	solution();
+}
